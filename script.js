94f5b7bd7cc4ac2f4d9da9845efbb27c08ae7464
Anthony Rossman <anthony.rossman0825@gmail.com>
2025-05-28 17:02:12 -0600

    add startRound function and getComputerChoice function

--- script.js
+++ script.js
@@ -1,35 +1,29 @@
 const userImages = [
   {
-    name: 'userRock',
-    nickName: 'rock',
+    name: 'rock',
     url: `./media/user-rock.png`
   },
   {
-    name: 'userPaper',
-    nickName: 'paper',
+    name: 'paper',
     url: `./media/user-paper.png`
   },
     {
-    name: 'userScissors',
-    nickName: 'scissors',
+    name: 'scissors',
     url: `./media/user-scissors.png`
   },
 ];
 
 const compImages = [
   {
-    name: 'compRock',
-    nickName: 'rock',
+    name: 'rock',
     url: `./media/comp-rock.png`
   },
   {
-    name: 'compPaper',
-    nickName: 'paper',
+    name: 'paper',
     url: `./media/comp-paper.png`
   },
   {
-    name: 'compScissors',
-    nickName: 'scissors',
+    name: 'scissors',
     url: `./media/comp-scissors.png`
   },
 ];
@@ -38,21 +32,26 @@ let userChoice = '';
 let compChoice = '';
 
 function getUserChoice(choice) {
-  userChoice = choice;
   let userHand = document.getElementById('user-hand');
   let userHandContent = '';
   let image = userImages.find((image) => image.name == choice);
-  userHandContent = `<img src="${image.url}" class="w-100">${image.nickName}`;
+  userHandContent = `<img src="${image.url}" class="w-100">${image.name}`;
   userHand.innerHTML = userHandContent;
   document.getElementById('header-text').innerHTML = 
   `<div>Are you sure? <button class="btn btn-rounded bg-primary text-center" onclick="startRound()">Start Round</button></div>`
 };
 
 function getComputerChoice() {
+  let compHand = document.getElementById('comp-hand');
+  let compHandContent = '';
   let randomIndex = Math.floor(Math.random() * 3);
   let indexChoice = compImages[randomIndex];
-  compChoice = indexChoice.nickName;
+  compChoice = indexChoice.name;
+  compHandContent = `<img src="${indexChoice.url}" class="w-100">${compChoice}`;
+  compHand.innerHTML = compHandContent;
 }
+
+
 
 function compareChoices() {
   
@@ -61,4 +60,7 @@ function compareChoices() {
 function startRound() {
   document.getElementById('choices-box').classList.remove('show-content');
   document.getElementById('choices-box').classList.add('hide-content');
+  getComputerChoice();
+  compareChoices();
+  declareWinner();
 };
